d05c230124225637a019edc4804e7da72a7079b6
Yuri Orikassa Simoes <yuri.orikassa@usp.br>
2026-05-02 12:26:48 -0300

    Teste com PWM + Input Capture funcionando

--- src/main.c
+++ src/main.c
@@ -2,28 +2,30 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/devicetree.h>
-#include "pwm_z42.h"                // API para controle de PWM
+#include "pwm_z42.h"   // API para controle de PWM
 
 #define TPM_IRQ_LINE TPM1_IRQn  // relaciona a interrupção ao timer TPM1
 #define TPM_IRQ_PRIORITY 1      // define a prioridade da interrupção
+
+#define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 
 volatile uint16_t captured= 0; 
 
 void tpm1_isr(void *arg)
 {
-       TPM1->STATUS |= TPM_STATUS_CH0F_MASK; // zerra a flag que gerou a interrupção
-
+      // TPM1->STATUS |= TPM_STATUS_CH0F_MASK; // zerra a flag que gerou a interrupção
+       TPM1->STATUS = TPM_STATUS_CH0F_MASK;
        captured = TPM1->CONTROLS[0].CnV; // coloca o valor atual do timer na variável "captured"
 }
 
-#define INPUT_PORT  "gpio@400ff0c0"   // Porta D configurada com endereço direto
-                                      // Pegue o endereço no zephyr.dts dentro de pio \ build
-#define INPUT_PIN7   7                // PTD7
-
 void main(void)
 {
-    int ret, val;  
-    const struct device *input_dev;   
+
+    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    
+    pwm_tpm_Ch_Init(TPM0, 1, TPM_PWM_H, GPIOD, 1);
+
+    pwm_tpm_CnV(TPM0, 1, 100); // Azul
     
     // Conecta a interrupção via Zephyr
     IRQ_CONNECT(TPM_IRQ_LINE, TPM_IRQ_PRIORITY, tpm1_isr, NULL, 0);
@@ -33,20 +35,11 @@ void main(void)
     pwm_tpm_Init(TPM1, TPM_PLLFLL, 65535, TPM_CLK, PS_128, EDGE_PWM);
 
     // Configura TPM1_CH0 como input capture na borda de subida
-    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_RISING | TPM_CHANNEL_INTERRUPT, GPIOD, 7);
-
-    input_dev = device_get_binding(INPUT_PORT);
-    if (!input_dev) {
-        printk("Erro ao acessar porta %s\n", INPUT_PORT);
-        return;
-    }
-    //ret = gpio_pin_configure(input_dev, INPUT_PIN7, GPIO_INPUT | GPIO_PULL_UP);
-
+    pwm_tpm_Ch_Init(TPM1, 0, TPM_INPUT_CAPTURE_RISING| TPM_CHANNEL_INTERRUPT, GPIOE, 20);
     while (1)
     {
         printk("Valor do TPM1: %u\n", captured);
-        val = gpio_pin_get(input_dev, INPUT_PIN7);
-        printk("Valor do PTD7: %d\n", val);
         k_msleep(1000); 
+
     }
 }
